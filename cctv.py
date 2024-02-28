9ef190eb04010e6bc8727df12acd52c66f88ec30
dong <csszue@gmail.com>
2024-02-28 08:51:00 +0000

    修改txt目录

--- cctv.py
+++ cctv.py
@@ -17,7 +17,7 @@ results = []
 channels = []
 error_channels = []
 
-with open("itv.txt", 'r', encoding='utf-8') as file:
+with open("tv/itv.txt", 'r', encoding='utf-8') as file:
     lines = file.readlines()
     for line in lines:
         line = line.strip()
@@ -101,7 +101,7 @@ now_today = datetime.date.today()
 
 result_counter = 8  # 每个频道需要的个数
 
-with open("cctv.txt", 'w', encoding='utf-8') as file:
+with open("tv/cctv.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
     file.write('央视频道,#genre#\n')
     for result in results:
@@ -117,20 +117,4 @@ with open("cctv.txt", 'w', encoding='utf-8') as file:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
 
-with open("cctv.m3u", 'w', encoding='utf-8') as file:
-    channel_counters = {}
-    file.write('#EXTM3U\n')
-    for result in results:
-        channel_name, channel_url, speed = result
-        if 'CCTV' in channel_name:
-            if channel_name in channel_counters:
-                if channel_counters[channel_name] >= result_counter:
-                    continue
-                else:
-                    file.write(f"#EXTINF:-1 group-title=\"央视频道\",{channel_name}\n")
-                    file.write(f"{channel_url}\n")
-                    channel_counters[channel_name] += 1
-            else:
-                file.write(f"#EXTINF:-1 group-title=\"央视频道\",{channel_name}\n")
-                file.write(f"{channel_url}\n")
-                channel_counters[channel_name] = 1
+
--- qita.py
+++ qita.py
@@ -17,7 +17,7 @@ results = []
 channels = []
 error_channels = []
 
-with open("itv.txt", 'r', encoding='utf-8') as file:
+with open("tv/itv.txt", 'r', encoding='utf-8') as file:
     lines = file.readlines()
     for line in lines:
         line = line.strip()
@@ -143,25 +143,13 @@ with open("qita.m3u", 'w', encoding='utf-8') as file:
 
 # 合并文件内容
 file_contents = []
-file_paths = ["cctv.txt", "weishi.txt", "qita.txt"]  # 替换为实际的文件路径列表
+file_paths = ["tv/cctv.txt", "tv/weishi.txt", "tv/qita.txt"]  # 替换为实际的文件路径列表
 for file_path in file_paths:
     with open(file_path, 'r', encoding="utf-8") as file:
         content = file.read()
         file_contents.append(content)
 
 # 写入合并后的文件
-with open("itvlist.txt", "w", encoding="utf-8") as output:
-    output.write('\n'.join(file_contents))
-    
-# 合并文件内容
-file_contents = []
-file_paths = ["cctv.m3u", "weishi.m3u", "qita.m3u"]  # 替换为实际的文件路径列表
-for file_path in file_paths:
-    with open(file_path, 'r', encoding="utf-8") as file:
-        content = file.read()
-        file_contents.append(content)
-
-# 写入合并后的文件
-with open("itvlist.m3u", "w", encoding="utf-8") as output:
+with open("tv/itvlist.txt", "w", encoding="utf-8") as output:
     output.write('\n'.join(file_contents))
     
