14a1ad6eec9c8b7bf86a584ca7184b8623165fb6
dong <csszue@gmail.com>
2024-03-01 03:48:28 +0000

    增加一些筛选

--- py/cctv.py
+++ py/cctv.py
@@ -101,12 +101,17 @@ now_today = datetime.date.today()
 
 result_counter = 8  # 每个频道需要的个数
 
+
+
+
+
 with open("tv/cctv.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
     file.write('央视频道,#genre#\n')
     for result in results:
         channel_name, channel_url, speed = result
-        if 'CCTV' in channel_name:
+        excluded_channels = ['俄语', '教育', '英语', '西班牙', '阿拉伯语', '音乐', '风云']
+        if 'CCTV' in channel_name and not any(excluded in channel_name for excluded in excluded_channels):
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
                     continue
