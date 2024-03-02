2652da63a75bbe7820a05850c7f8ab9aa1f542b6
dong <csszue@gmail.com>
2024-03-02 03:00:39 +0000

    111111111111

--- py/itv.py
+++ py/itv.py
@@ -1,151 +1,75 @@
 import time
 import os
-
+import concurrent.futures
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import requests
 import re
-from urllib.parse import urlparse, urlunparse
-from concurrent.futures import ThreadPoolExecutor
-import concurrent.futures
 
-# ...
+# 将URL和文件名提取为变量，以便于管理和修改
+URLS_FILE = "py/urls.txt"
+RESULTS_FILE = "py/results.txt"
 
+def get_urls_from_file(file):
+    with open(file, 'r') as f:
+        return [line.strip() for line in f]
 
+def write_results_to_file(file, results):
+    with open(file, 'w') as f:
+        for result in results:
+            f.write(result + "\n")
 
+def get_page_content(url):
+    # 创建一个Chrome WebDriver实例
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
 
+    with webdriver.Chrome(options=chrome_options) as driver:
+        # 使用WebDriver访问网页
+        driver.get(url)
+        time.sleep(10)
+        # 获取网页内容
+        return driver.page_source
 
-
-
-session = requests.Session()
-
-urls = [
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iSGViZWki",  # Hebei (河北)
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iYmVpamluZyI%3D",  # Beijing (北京)
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iZ3Vhbmdkb25nIg%3D%3D",  # Guangdong (广东)
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0ic2hhbmdoYWki",  # Shanghai (上海)
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0idGlhbmppbiI%3D",  # Tianjin (天津)
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iY2hvbmdxaW5nIg%3D%3D",  # Chongqing (重庆)
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0ic2hhbnhpIg%3D%3D",  # Shanxi (山西)
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iU2hhYW54aSI%3D",  # Shaanxi (陕西)
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0ibGlhb25pbmci",  # Liaoning (辽宁)
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iamlhbmdzdSI%3D",  # Jiangsu (江苏)
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iemhlamlhbmci",  # Zhejiang (浙江)
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5a6J5b69Ig%3D%3D",  # Anhui (安徽)
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iRnVqaWFuIg%3D%3D",  # 福建
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rGf6KW%2FIg%3D%3D",  # 江西
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5bGx5LicIg%3D%3D",  # 山东
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rKz5Y2XIg%3D%3D",  # 河南
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rmW5YyXIg%3D%3D",  # 湖北
-    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rmW5Y2XIg%3D%3D",  # 湖南
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22hebei%22",        #河北
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22beijing%22",   #北京
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22guangdong%22",    #广东
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22shanghai%22",    #上海
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22tianjin%22",    #天津
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22chongqing%22",    #重庆
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22shanxi%22",    #山西
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22shaanxi%22",    #陕西
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22liaoning%22",    #辽宁
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22jiangsu%22",    #江苏
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22zhejiang%22",    #浙江
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22anhui%22",    #安徽
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22fujian%22",    #福建
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22jiangxi%22",    #江西
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22shandong%22",    #山东
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22henan%22",    #河南
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22hubei%22",    #湖北
-    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22hunan%22"    #湖南
-    ]
-
-
-def modify_and_check_urls(url):
-    parsed_url = urlparse(url)
-    base_url = parsed_url.scheme + "://" + parsed_url.netloc
-    ip_end = "/iptv/live/1000.json?key=txiptv"
-    modified_urls = []
-    for i in range(1, 256):
-        modified_ip = f"{parsed_url.hostname[:-1]}{i}"
-        modified_url = f"{base_url}{modified_ip}{parsed_url.port}{ip_end}"
-        if is_url_accessible(modified_url):
-            modified_urls.append(modified_url)
-    
-    return modified_urls
-
+def get_urls_from_content(content):
+    # 查找所有符合指定格式的网址
+    pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"
+    return set(re.findall(pattern, content))
 
 def is_url_accessible(url):
     try:
-        response = session.get(url, timeout=0.5)
-        return response.status_code == 200
+        response = requests.get(url, timeout=0.5)
+        if response.status_code == 200:
+            return url
     except requests.exceptions.RequestException:
-        return False
+        pass
+    return None
 
-# ...
+def get_valid_urls(urls):
+    valid_urls = []
+    # 使用并发处理来提高效率
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        futures = [executor.submit(is_url_accessible, url) for url in urls]
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            if result:
+                valid_urls.append(result)
+    return valid_urls
 
-# ...
+def main():
+    urls = get_urls_from_file(URLS_FILE)
+    results = []
+    for url in urls:
+        try:
+            content = get_page_content(url)
+            urls_from_content = get_urls_from_content(content)
+            valid_urls = get_valid_urls(urls_from_content)
+            results.extend(valid_urls)
+        except Exception as e:
+            print(f"Error processing url {url}: {e}")
+    write_results_to_file(RESULTS_FILE, results)
 
-name = ""
-
-valid_urls = []
-
-# ...
-with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-    futures = [executor.submit(modify_and_check_urls, url) for url in urls]
-    for future in concurrent.futures.as_completed(futures):
-        valid_urls.extend(future.result())
-
-# ...
-        
-name_replacements = {
-    "cctv": "CCTV",
-    "中央": "CCTV",
-    "央视": "CCTV",
-    "高清": "",
-    "超高": "",
-    "HD": "",
-    "标清": "",
-    "频道": "",
-    "-": "",
-    " ": "",
-    "PLUS": "+",
-    "＋": "+",
-    "(": "",
-    ")": "",
-    "CCTV1综合": "CCTV1",
-    "CCTV2财经": "CCTV2",
-    "CCTV3综艺": "CCTV3",
-    "CCTV4国际": "CCTV4",
-    "CCTV4中文国际": "CCTV4",
-    "CCTV4欧洲": "CCTV4",
-    "CCTV5体育": "CCTV5",
-    "CCTV6电影": "CCTV6",
-    "CCTV7军事": "CCTV7",
-    "CCTV7军农": "CCTV7",
-    "CCTV7农业": "CCTV7",
-    "CCTV7国防军事": "CCTV7",
-    "CCTV8电视剧": "CCTV8",
-    "CCTV9记录": "CCTV9",
-    "CCTV9纪录": "CCTV9",
-    "CCTV10科教": "CCTV10",
-    "CCTV11戏曲": "CCTV11",
-    "CCTV12社会与法": "CCTV12",
-    "CCTV13新闻": "CCTV13",
-    "CCTV新闻": "CCTV13",
-    "CCTV14少儿": "CCTV14",
-    "CCTV15音乐": "CCTV15",
-    "CCTV16奥林匹克": "CCTV16",
-    "CCTV17农业农村": "CCTV17",
-    "CCTV17农业": "CCTV17",
-    "CCTV5+体育赛视": "CCTV5+",
-    "CCTV5+体育赛事": "CCTV5+",
-    "CCTV5+体育": "CCTV5+"
-}
-
-for old, new in name_replacements.items():
-    name = name.replace(old, new)
-
-name = re.sub(r"CCTV(\d+)台", r"CCTV\1", name)
-results = sorted(set(valid_urls))
-with open("tv/itv.txt", 'w', encoding='utf-8') as file:
-    file.writelines(result + "\n" for result in results)
-    print("\n".join(results))
+if __name__ == "__main__":
+    main()
